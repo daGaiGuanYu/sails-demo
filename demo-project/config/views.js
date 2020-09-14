f9cbd899d55bd1e7a069a67798ec72fc7aab3aa1
gy <372301467@qq.com>
2020-09-14 18:00:12 +0800

    使用 pug

--- demo-project/config/views.js
+++ demo-project/config/views.js
@@ -1,41 +1,7 @@
-/**
- * View Engine Configuration
- * (sails.config.views)
- *
- * Server-sent views are a secure and effective way to get your app up
- * and running. Views are normally served from actions.  Below, you can
- * configure your templating language/framework of choice and configure
- * Sails' layout support.
- *
- * For details on available options for configuring server-side views, check out:
- * https://sailsjs.com/config/views
- *
- * For more background information on views and partials in Sails, check out:
- * https://sailsjs.com/docs/concepts/views
- */
-
 module.exports.views = {
-
-  /***************************************************************************
-  *                                                                          *
-  * Extension to use for your views. When calling `res.view()` in an action, *
-  * you can leave this extension off. For example, calling                   *
-  * `res.view('homepage')` will (using default settings) look for a          *
-  * `views/homepage.ejs` file.                                               *
-  *                                                                          *
-  ***************************************************************************/
-
-  // extension: 'ejs',
-
-  /***************************************************************************
-  *                                                                          *
-  * The path (relative to the views directory, and without extension) to     *
-  * the default layout file to use, or `false` to disable layouts entirely.  *
-  *                                                                          *
-  * Note that layouts only work with the built-in EJS view engine!           *
-  *                                                                          *
-  ***************************************************************************/
-
-  layout: 'layouts/layout'
-
-};
+  // layout: 'layouts/layout', 
+  extension: 'pug', // extension 似乎和 getRenderFn 有重复的嫌疑，但事实上，这个配置只说明 view 文件的拓展名。所以，即使使用 pug，也可以使用 ejs 后缀。
+  getRenderFn(){ // 模板引擎非常多，因此这里单独搞一个方法，作为适配器
+    return require('consolidate').pug
+  }
+}
